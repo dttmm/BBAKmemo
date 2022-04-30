4419019a657b7ccbb8cec0673a237be80bc12ff0
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-04-30 20:37:21 +0900

    ✨ feat : 저장, 삭제, 취소 버튼 구현
    EditText에 two-way binding 적용

--- app/src/main/java/com/dttmm/bbakmemo/memo/MemoEditActivity.kt
+++ app/src/main/java/com/dttmm/bbakmemo/memo/MemoEditActivity.kt
@@ -3,9 +3,11 @@ package com.dttmm.bbakmemo.memo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.dttmm.bbakmemo.R
 import com.dttmm.bbakmemo.databinding.ActivityMemoEditBinding
+import com.dttmm.bbakmemo.dto.MemoDto
 import com.dttmm.bbakmemo.repository.MemoRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +21,18 @@ class MemoEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_memo_edit)
+
+        binding.btnSave.setOnClickListener {
+            saveMemo()
+        }
+
+        binding.btnDelete.setOnClickListener {
+            deleteMemo()
+        }
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
 
     }
 
@@ -34,5 +48,39 @@ class MemoEditActivity : AppCompatActivity() {
             binding.btnDelete.visibility = View.GONE
             binding.etContent.requestFocus()
         }
+    }
+
+    private fun saveMemo() {
+        if (check()) {
+            if (id >= 0) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    MemoRepository.getInstance().update(binding.memo as MemoDto)
+                }
+            } else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    MemoRepository.getInstance().insert(
+                        MemoDto(
+                            content = binding.etContent.text.toString(),
+                            password = binding.etPassword.text.toString()
+                        )
+                    )
+                }
+            }
+            finish()
+        }
+    }
+
+    private fun deleteMemo() {
+        CoroutineScope(Dispatchers.IO).launch {
+            MemoRepository.getInstance().delete(binding.memo as MemoDto)
+        }
+        finish()
+    }
+
+    private fun check(): Boolean {
+        if (binding.etContent.text.isNullOrBlank()) {
+            Toast.makeText(this, "내용을 입력해주세요", Toast.LENGTH_SHORT).show()
+            return false
+        } else return true
     }
 }
